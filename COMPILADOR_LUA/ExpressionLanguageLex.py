38d7385ec658dc85abcd02448813faed07d58178
Magda Amaral <31852144+tainy2017@users.noreply.github.com>
2023-03-22 23:47:45 +0000

    continua...

--- COMPILADOR_LUA/ExpressionLanguageLex.py
+++ COMPILADOR_LUA/ExpressionLanguageLex.py
@@ -23,7 +23,9 @@ reserved = {
     'return': 'RETURN',
     'then': 'THEN',
     'until': 'UNTIL',
-    'while': 'WHILE'
+    'while': 'WHILE',
+    'var' : 'VAR',
+    'string': 'STRING',
  }
 # Lista de tokens
 tokens = [
@@ -44,8 +46,13 @@ tokens = [
     'DOT',
     'VARARGS',
     'UNTIL',
-    'LOCAL',
- 
+    'LOCAL', 
+    'ATRIB',
+    'DIF',
+    'GT',
+    'LT',
+    'GTEQUALS',
+    'LTEQUALS', 
  ] + list(reserved.values()) 
 
 # Regras de expressões regulares para tokens simples
@@ -53,7 +60,9 @@ t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_EQUALS = r'='
+t_ATRIB = r'='
+t_EQUALS = r'=='
+t_DIF = r'~='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_LBRACE = r'{'
@@ -61,8 +70,16 @@ t_RBRACE = r'}'
 t_COMMA = r','
 t_SEMICOLON = r';'
 t_COLON = r':'
+t_COLON = r'::'
+t_AND = r'%'
 t_DOT = r'\.'
 t_VARARGS = r'\.\.\.'
+t_LCOLCH  = r'\['
+t_RCOLCH  = r'\]'
+t_GT = r'>'
+t_LT = r'<'
+t_GTEQUALS = r'>='
+t_LTEQUALS = r'<='
 
 # Nomes e números
 def t_NAME(t):
@@ -81,7 +98,17 @@ def t_COMMENT(t):
     pass
 
 # Ignorar espaços em branco
-t_ignore = " \t"
+t_ignore  = ' \t'
+
+def t_ID(t):
+  r'[a-zA-Z_][a-zA-Z_0-9]*'
+  t.type = reserved.get(t.value.lower(),'ID') 
+  return t
+
+def t_STRING(t): 
+  r'\"(.|\n)*?\"'
+  t.type = reserved.get(t.value.lower(),'STRING') 
+  return t
 
 # Erro
 def t_error(t):
--- COMPILADOR_LUA/sintatico.py
+++ COMPILADOR_LUA/sintatico.py
@@ -44,6 +44,81 @@ def p_ReturnStmtm(p):
   ''' ReturnStmt : RETURN ExpressionList
   | RETURN '''
 
+def p_add_op(p):
+  ''' add_op : PLUS
+  | MINUS'''
+
+def p_mul_op(p):
+  ''' mul_op : TIMES
+  | DIVIDE
+  | MODULE 
+  | LSHIFT
+  | RSHIFT '''
+  
+def p_comando(p):
+  ''' comando ::= listavars EQUALS listaexps ''' 
+  
+def p_listavars(p):
+  ''' listavars ::= var LBRACE COMMA var RBRACE '''
+
+def p_listaexps(p):
+  ''' listaexps ::= exp LBRACE COMMA exp RBRACE '''  
+
+def p_var(p):
+  ''' var ::= var  '''
+
+def p_Declaration(p):
+  ''' Declaration : ConstDecl  
+  | VarDecl '''   
+
+def p_VarDecl(p):
+  ''' VarDecl : VAR VarSpec 
+  | LPAREN VarDecll RPAREN 
+  | LPAREN RPAREN '''
+
+def p_VarDecll(p):
+  ''' VarDecll : VarSpec SEMICOLON VarDecll
+  | VarSpec SEMICOLON '''
+
+def p_VarSpec(p):
+  ''' VarSpec : IdentifierList Type
+  | IdentifierList Type EQ ExpressionList
+  | EQ ExpressionList '''  
+  
+def p_PrimaryExpr(p):
+  ''' PrimaryExpr : Literal
+  | ID
+  | LPAREN Expression RPAREN
+  | ID LCOLCH Expression RCOLCH
+  | ID Arguments '''
+
+def p_Expression(p):
+  #''' Expression : IF '''
+  ''' Expression : Expression binary_op Expression1
+  | Expression1 '''
+
+def p_Expression1(p):
+  ''' Expression1 : UnaryExpr '''  
+
+def p_Arguments(p):
+  ''' Arguments : LPAREN ExpressionList RPAREN
+  | LPAREN Type RPAREN 
+  | LPAREN TYPE COMMA ExpressionList RPAREN
+  | LPAREN RPAREN '''    
+
+def p_Literal(p):
+  ''' Literal : NUMBER
+  | STRING
+  | TRUE
+  | FALSE '''  
+
+def p_trecho(p):
+  ''' trecho ::= bloco '''
+  
+def p_bloco(p):
+  ''' bloco :: LBRACE StatementList RBRACE 
+  | LBRACE RBRACE''' 
+ 
 def p_error(p):
   print('Error') 
   
