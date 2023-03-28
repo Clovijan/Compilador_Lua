f3e9e60ccea1dd3167ad155c657e00598687512b
Clovijan Rocha <clovijan@gmail.com>
2023-03-28 11:25:36 +0000

    config de builder

--- COMPILADOR_LUA/main.py
+++ COMPILADOR_LUA/main.py
@@ -1,8 +1,11 @@
 # ------------------------------------------------------------
 
 # ------------------------------------------------------------
-import ply.lex as lex
-from ExpressionLanguageLex import *
+#import ply.lex as lex
+#from ExpressionLanguageLex import *
 
 #parser = yacc.yacc()
 #parser.parse()
+
+python = "Ola mundo"
+print(python)
