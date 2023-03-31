fce030d5048a9cc15b48c4c38aabfb6dcb3b4512
Clovijan Rocha <clovijan@gmail.com>
2023-03-31 23:59:14 +0000

    correções na chamdas de teste

--- COMPILADOR_LUA/ExpressionLanguageParser.py
+++ COMPILADOR_LUA/ExpressionLanguageParser.py
@@ -1,179 +1,247 @@
 import ply.yacc as yacc
 from ExpressionLanguageLex import *
+import SintaxeAbstrata as sa
+import Visitor as vis
+import SemanticVisitor as sv
 
+# definição de trecho
 def p_program(p):
-  '''program : comands
-             | var_global program
-             | function
-             | local_function
-             | comands program
-             | function program
-             | local_function program'''
+    '''program : block'''
 
-def p_var_global(p):
-  '''var : NAME ATRIB exp_16'''
 
-def p_function(p):
-  '''function : FUNCTION signature comands END '''
-  
-def p_local_function(p):
-  '''local_function : LOCAL FUNCTION signature comands END'''
-  
-def p_signature(p):
-  '''signature : ID LPAREN sigparam RPAREN'''
-  
-def p_sigparam(p):
-  '''sigparam : ID
-              | n_sigparam'''
+# definição de bloco
+def p_block(p):
+    '''block : command
+             | command command_ret'''
 
-def p_n_sigparam(p):
-  '''n_sigparam : ID COMMA sigparam'''
 
-def p_comands(p):
-  '''comands : comand
-             | comand comands'''
+# definição de comando
+def p_command(p):
+    '''command : COMMA
+               | list_vars ATRIB list_exps
+               | call_function
+               | rotulo
+               | BREAK
+               | GOTO NAME
+               | DO block END
+               | struct_while
+               | struct_repeat
+               | struct_if_Else
+               | struct_for
+               | struct_for_in
+               | function
+               | local_function
+               | local_var'''
 
-def p_comand(p):
-  '''comand : comand_1
-            | comand_2'''
-    
-def p_comand_1(p):
-  '''comand_1 : IF list_exp THEN comand_1  ELSE comand_1 END 
-              | IF list_exp THEN comands ELSE comands END
-              | IF list_exp THEN comand_1 ELSE comands END
-              | IF list_exp THEN comands ELSE comand_1 END
-              | other_comand'''
-  
-#def p_elseif(p):
-#  '''elseif : ELSEIF list_exp THEN comands
-#            | '''
-  
-def p_other_comand(p):
-  '''other_comand : WHILE list_exp DO comands END
-                  | fordo END
-                  | forin END
-                  | exp
-                  | RETURN exp
-                  | BREAK'''
 
-def p_comand_2(p):
-  '''comand_2 : IF list_exp THEN comands END
-              | IF list_exp THEN comand_1 ELSE comand_2 END
-              | IF list_exp THEN comands ELSE comand_2 END'''
+# definição de comandoret
+def p_ret_command(p):
+    '''ret_command : RETURN LCOLCH list_exps RCOLCH
+                   | RETURN LCOLCH list_exps RCOLCH COMMA'''
 
-def p_fordo(p):
-  '''fordo : FOR exp DO comands
-           | END'''
 
-def p_forin(p):
-  '''forin : FOR exp IN comands'''
+#definição de comandret
+def p_command_ret(p):
+    '''command_ret : RETURN
+                 | RETURN list_exps
+                 | RETURN list_exps COMMA'''
 
-def p_list_exp(p):
-  '''list_exp : exp COMMA list_exp
-              | exp list_exp
-              | paren_list_exp COMMA list_exp
-              | exp 
-              | paren_list_exp
-              | paren_list_exp list_exp'''
 
-def p_paren_exp(p):
-  '''paren_list_exp : LPAREN exp RPAREN'''
- 
-def p_call(p):
-  '''call : ID paren_list_exp
-          | ID LPAREN RPAREN'''
+# definição de rótulo
+def p_rotulo(p):
+    '''rotulo : DUALCOLON NAME DUALCOLON'''
 
-#FALTA algo na definição dos arrays
-def p_array(p):
-  '''array : ID EQUAL LKEY exp RKEY
-           | ID EQUAL LKEY RKEY
-           | ID LCOCHETE ID RCOCHETE EQUAL exp'''
 
+# definicao de nomefuncao
+def name_fuction(p):
+    '''name_function : NAME
+                     | NAME DOT NAME
+                     | NAME COLON NAME'''
+
+
+# definição de listavars
+def p_list_vars(p):
+    '''list_vars : var 
+                 | var SEMICOLON list_vars'''
+
+
+# definição de var
+def p_var(p):
+    '''var : NAME 
+           | prefix_exp LCOLCH exp RCOLCH
+           | prefix_exp DOT NAME'''
+
+
+# definição de listanomes
+def p_list_names(p):
+    '''list_names: NAME 
+                 | NAME COMMA list_names'''
+
+
+# definição de listaexps
+def p_list_exps(p):
+    '''list_exps : exp 
+                 | exp COMMA list_exps'''
+
+
+# definição de exp
 def p_exp(p):
-    '''exp : exp ASSIGN exp_1
-           | exp_1'''
-          
-def p_exp_1(p):
-  '''exp_1 : exp_1 OR exp_2
-           | exp_1 BITWISE_OR exp_2
-           | exp_2'''
-   
-def p_exp_2(p): 
-  '''exp_2 : exp_2 AND exp_3
-           | exp_2 BITWISE_AND exp_3
-           | exp_3'''
-  
-def p_exp_3(p):
-  '''exp_3 : exp_3 BITWISE_EXC_OR exp_4
-           | exp_4'''
+    '''exp: NIL
+          | FALSE
+          | TRUE
+          | NUMBER
+          | CADEIA
+          | VARARGS
+          | def_function
+          | exp_prefix
+          | construct_table
+          | exp op_bin exp
+          | op_unaria exp'''
 
-def p_exp_4(p):
-  '''exp_4 : exp_4 LESS_THAN exp_5
-           | exp_4 BIGGER_THAN exp_5
-           | exp_4 BIGGER_EQUAL exp_5
-           | exp_4 LESS_EQUAL exp_5
-           | exp_4 DIF exp_5
-           | exp_4 EQUAL exp_5
-           | exp_5'''
 
-def p_exp_5(p):
-  '''exp_5 : exp_5 CONCATENATION exp_6
-           | exp_6'''
+# definição de expprefixo
+def p_exp_prefix(p):
+    '''exp_prefix : VAR 
+                  | call_function
+                  | LPAREN exp RPAREN'''
 
-def p_exp_6(p):
-  '''exp_6 : exp_6 SUB exp_7
-           | exp_7'''
 
-def p_exp_7(p):
-  '''exp_7 : exp_7 SUM exp_8
-           | exp_8'''
+# definição de chamadafuncao
+def p_call_function(p):
+    '''call_function: exp_prefix args
+                    | exp_prefix COLON NAME args'''
 
-def p_exp_8(p):
-  '''exp_8 : exp_8 MODULE exp_9
-            | exp_9'''
 
-def p_exp_9(p):
-  '''exp_9 : exp_9 DIV exp_10
-            | exp_10'''
-  
-def p_exp_10(p):
-  '''exp_10 : exp_10 DIV_INT exp_11
-            | exp_11'''
+# definição de args
+def p_args(p):
+    ''' args : LPAREN list_exp RPAREN
+              | construct_table
+              | cadeia'''
 
-def p_exp_11(p):
-  '''exp_11 : exp_11 MULT exp_12
-            | exp_12'''
 
-def p_exp_12(p):
-  '''exp_12 : exp_12 NOT exp_13
-            | exp_13'''
+# definição de deffunção
+def p_def_function(p):
+    '''def_function : FUNCTION body_function'''
 
-def p_exp_13(p):
-  '''exp_13 : DENIAL exp_13
-            | UNARY_BITWISE_NOT exp_13
-            | LENGHT exp_13
-            | exp_14'''
-  
-def p_exp_14(p):
-  '''exp_14 : exp_14 POW exp_15
-            | exp_15'''
-  
-def p_exp_15(p):
-  '''exp_15 : exp_15 COMMA exp_16
-            | exp_15 DOT exp_16
-            | LOCAL exp_15
-            | call
-            | array
-            | exp_16'''
 
-def p_exp_16(p):
-  '''exp_16 : NUMBER 
-            | STRING 
-            | ID
-            | NIL
-            | TRUE
-            | FALSE'''
-            
+# definição de corpofunção
+def p_body_function(p):
+    '''body_function : LPAREN list_pars RPAREN block END'''
+
+
+# definição de listapars
+def p_list_pars(p):
+    '''list_pars : list_names COMMA VARARGS
+                 | VARARGS
+                 | list_names'''
+
+
+# definição de construtortabela
+def p_construct_table(p):
+    '''construct_table : LBRACE list_fields RBRACE'''
+
+
+# definição de listadecampos
+def p_list_fields(p):
+    '''list_fields : field LBRACE separator_fields field RPARECE
+                   | separator_fields'''
+
+
+# definição de campo
+def p_field(p):
+    '''field : LCOLCH exp RCOLCH ATRIB exp
+             | NAME ATRIB exp
+             | exp'''
+
+
+# definição de separadordecampos
+def p_separator_fields(p):
+    '''separator_fields : COMMA
+                        | SEMICOLON'''
+
+
+# definição de variável local
+def p_local_var(p):
+    '''local_var : LOCAL list_names ATRIB list_exp'''
+
+
+# definição de opbin
+def p_op_bin(p):
+    ''' op_bin : PLUS
+               | MINUS
+               | TIMES
+               | DIVIDE
+               | EXPO 
+               | PERCENTUAL
+               | CONCAT
+               | LT
+               | LTEQUALS
+               | GT
+               | GTEQUALS
+               | EQUALS
+               | DIF
+               | AND
+               | OR'''
+
+
+# definição de opunária
+def p_op_unary(p):
+    '''op_unary : MINUS
+                | not
+                | TAG'''
+
+
+# definicao de função
+def p_function(p):
+    '''function : FUNCTION name_function body_function'''
+
+
+def p_if(p):
+    '''if : IF exp THEN block else_ifs else END'''
+
+
+def p_else_ifs(p):
+    '''else_ifs : else_ifs else_if
+              | else_if'''
+
+
+def p_else_if(p):
+    '''else_if : ELSEIF exp THEN block'''
+
+
+def p_else(p):
+    '''else : ELSE block'''
+
+
+# definicao de while
+def p_struct_while(p):
+    '''struct_while : WHILE exp DO block END'''
+
+
+# definicao de for
+def p_struct_for(p):
+    '''struct_for : FOR NAME ATRIB exp COMMA exp DO block END
+                  | FOR NAME ATRIB exp COMMA exp COMMA exp DO block END'''
+
+
+# definicao de forin
+def p_struct_for_in(p):
+    '''struct_for_in : FOR list_names IN listExp DO block END'''
+
+
+# definição de repeat
+def p_struct_repeat(p):
+    '''struct_repeat : REPEAT block UNTIL exp'''
+
+
+# definicao de funcao local
+def p_local_function(p):
+    '''local_function : FUCTION name_function body_function'''
+
+
 def p_error(p):
-    print("Erro Sintático") 
-         
+    print("Syntax error in input!")
+
+
+# REFERÊNCIAS (NO FINAL DO MANUAL TEM A DEFINIÇÃO DE TUDO)
+# LINK DO MANUAL : https://www.lua.org/manual/5.2/pt/manual.html
--- COMPILADOR_LUA/main.py
+++ COMPILADOR_LUA/main.py
@@ -1,8 +1,43 @@
-# ------------------------------------------------------------
+from ExpressionLanguageLex import *
+from ExpressionLanguageParser import *
 
-# ------------------------------------------------------------
-#import ply.lex as lex
-#from ExpressionLanguageLex import *
+lexer = lex.lex()
 
-#parser = yacc.yacc()
-#parser.parse()
+data = '''
+local a, b = 10, 20
+local tabela = {nome = "João", idade = 30}
+
+-- Definição de função
+function soma(x, y)
+    return x + y
+end
+
+-- Declaração de bloco de controle
+if a > b then
+    print("a é maior do que b")
+elseif b > a then
+    print("b é maior do que a")
+else
+    print("a e b são iguais")
+end
+
+tabela.ocupacao = "programador"
+
+local resultado = soma(a, b)
+
+print("O resultado da soma é:", resultado)
+print("O nome é:", tabela.nome)
+print("A idade é:", tabela.idade)
+print("A ocupação é:", tabela.ocupacao)
+'''
+lexer.input(data)
+parser = yacc.yacc(start='program')
+result = parser.parse(debug=False)
+print("#realiza a analise semantica")
+
+visitor = sv.SemanticVisitor()
+
+print('#imprime o programa passado como entrada')
+visitor = vis.Visitor()
+for r in result:
+    r.accept(visitor)
